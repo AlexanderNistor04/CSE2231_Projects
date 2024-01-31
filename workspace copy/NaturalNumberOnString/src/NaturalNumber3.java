677e81ff1edfc786e5bda01e0b496ab050a91349
AA-maker <57161843+AA-maker@users.noreply.github.com>
2024-01-30 21:28:45 -0500

    Update NaturalNumber3.java

--- workspace copy/NaturalNumberOnString/src/NaturalNumber3.java
+++ workspace copy/NaturalNumberOnString/src/NaturalNumber3.java
@@ -14,7 +14,7 @@ import components.naturalnumber.NaturalNumberSecondary;
  *         else the decimal number whose ordinary depiction is $this.rep]
  * </pre>
  *
- * @author Put your name here
+ * @author Akshay Anand and Alexander Nistor
  *
  */
 public class NaturalNumber3 extends NaturalNumberSecondary {
@@ -33,7 +33,8 @@ public class NaturalNumber3 extends NaturalNumberSecondary {
      */
     private void createNewRep() {
 
-        // TODO - fill in body
+        //Creates "zero" string
+        this.rep = "";
 
     }
 
@@ -46,7 +47,8 @@ public class NaturalNumber3 extends NaturalNumberSecondary {
      */
     public NaturalNumber3() {
 
-        // TODO - fill in body
+        //Creates zero string
+        this.createNewRep();
 
     }
 
@@ -59,7 +61,12 @@ public class NaturalNumber3 extends NaturalNumberSecondary {
     public NaturalNumber3(int i) {
         assert i >= 0 : "Violation of: i >= 0";
 
-        // TODO - fill in body
+        //Checks if inputed integer is 0 to create integer string or empty string
+        if (i != 0) {
+            this.rep = String.valueOf(i);
+        } else {
+            this.createNewRep();
+        }
 
     }
 
@@ -74,7 +81,13 @@ public class NaturalNumber3 extends NaturalNumberSecondary {
         assert s.matches("0|[1-9]\\d*") : ""
                 + "Violation of: there exists n: NATURAL (s = TO_STRING(n))";
 
-        // TODO - fill in body
+        //If-statement checks if string is 0 to make empty string or store
+        //inputed string into this.rep
+        if (!s.equals("0")) {
+            this.rep = s;
+        } else {
+            this.createNewRep();
+        }
 
     }
 
@@ -87,7 +100,16 @@ public class NaturalNumber3 extends NaturalNumberSecondary {
     public NaturalNumber3(NaturalNumber n) {
         assert n != null : "Violation of: n is not null";
 
-        // TODO - fill in body
+        //Converts nn to string and stores it
+        String nn = n.toString();
+
+        //If-statement checks if inputed nn is 0 before converting to empty string
+        //or string with nn's value
+        if (!nn.equals("0")) {
+            this.rep = nn;
+        } else {
+            this.createNewRep();
+        }
 
     }
 
@@ -134,26 +156,43 @@ public class NaturalNumber3 extends NaturalNumberSecondary {
         assert 0 <= k : "Violation of: 0 <= k";
         assert k < RADIX : "Violation of: k < 10";
 
-        // TODO - fill in body
+        //If-statement checks if current string's lenght is 0 before adding onto it
+        //or assigning it an empty string value or integer string value
+        if (this.rep.length() != 0) {
+            this.rep += "" + k;
+        } else {
+            if (k == 0) {
+                this.createNewRep();
+            } else {
+                this.rep = Integer.toString(k);
+            }
+        }
 
     }
 
     @Override
     public final int divideBy10() {
 
-        // TODO - fill in body
+        //Initializes variable to store remainder value
+        int num = 0;
 
-        // This line added just to make the component compilable.
-        return 0;
+        //If-statement checks length of string to see whether to shorten it or not
+        if (this.rep.length() > 0) {
+            //Stores last character (digit) in variable to be returned
+            num = Integer.parseInt(this.rep.substring(this.rep.length() - 1));
+
+            //Sets string to be its same value, with the last digit cut off
+            this.rep = this.rep.substring(0, this.rep.length() - 1);
+        }
+
+        return num;
     }
 
     @Override
     public final boolean isZero() {
 
-        // TODO - fill in body
-
-        // This line added just to make the component compilable.
-        return false;
+        //Returns true or false if length of string is 0 indicating NN is 0
+        return this.rep.length() == 0;
     }
 
 }
