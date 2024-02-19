cdd60911bbcc036f2fe63bce47b994256e920801
AlexanderNistor04 <99147013+AlexanderNistor04@users.noreply.github.com>
2024-02-19 15:03:09 -0500

    Update SetTest.java
    created test cases for kernel methods and default constructor

--- workspace copy/SetOnBST/test/SetTest.java
+++ workspace copy/SetOnBST/test/SetTest.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 import components.set.Set;
@@ -5,7 +8,7 @@ import components.set.Set;
 /**
  * JUnit test fixture for {@code Set<String>}'s constructor and kernel methods.
  *
- * @author Put your name here
+ * @author Alexander Nistor & Akshay Anand
  *
  */
 public abstract class SetTest {
@@ -72,11 +75,276 @@ public abstract class SetTest {
 
     //--------------- constructor tests ---------------
     /**
-     * test of empty constructor
+     * test of empty constructor.
      */
     @Test
-    public final void test_constructor_empty() {
+    public final void testConstructorEmpty() {
+        Set<String> s1 = this.constructorRef();
+        Set<String> s2 = this.constructorTest();
+        assertEquals(s1, s2);
+    }
 
+    //--------------- add tests ---------------
+    /**
+     * test of add("a") with this = <>.
+     */
+    @Test
+    public final void testAdd1() {
+        Set<String> s1 = this.createFromArgsRef("a");
+        Set<String> s2 = this.createFromArgsTest();
+        s2.add("a");
+        assertEquals(s1, s2);
+    }
+
+    /**
+     * test of add("a") with this = <"b">.
+     */
+    @Test
+    public final void testAdd2() {
+        Set<String> s1 = this.createFromArgsRef("a", "b");
+        Set<String> s2 = this.createFromArgsTest("b");
+        s2.add("a");
+        assertEquals(s1, s2);
+    }
+
+    /**
+     * test of add("a") with this = <"b", "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testAdd3() {
+        Set<String> s1 = this.createFromArgsRef("a", "b", "c", "d", "e", "f",
+                "g");
+        Set<String> s2 = this.createFromArgsTest("b", "c", "d", "e", "f", "g");
+        s2.add("a");
+        assertEquals(s1, s2);
+    }
+
+    /**
+     * test of add(<"a">) with this = <>.
+     */
+    @Test
+    public final void testAdd4() {
+        Set<String> s1 = this.createFromArgsRef("a");
+        Set<String> s2 = this.createFromArgsTest();
+        Set<String> s3 = this.createFromArgsRef("a");
+        s2.add(s3);
+        assertEquals(s1, s2);
+    }
+
+    /**
+     * test of add(<"a">) with this = <"b">.
+     */
+    @Test
+    public final void testAdd5() {
+        Set<String> s1 = this.createFromArgsRef("a", "b");
+        Set<String> s2 = this.createFromArgsTest("b");
+        Set<String> s3 = this.createFromArgsRef("a");
+        s2.add(s3);
+        assertEquals(s1, s2);
+    }
+
+    /**
+     * test of add(<"a">) with this = <"b", "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testAdd6() {
+        Set<String> s1 = this.createFromArgsRef("a", "b", "c", "d", "e", "f",
+                "g");
+        Set<String> s2 = this.createFromArgsTest("b", "c", "d", "e", "f", "g");
+        Set<String> s3 = this.createFromArgsRef("a");
+        s2.add(s3);
+        assertEquals(s1, s2);
+    }
+
+    /**
+     * test of add(<"b", "c", "d", "e", "f", "g">) with this = <"a">.
+     */
+    @Test
+    public final void testAdd7() {
+        Set<String> s1 = this.createFromArgsRef("a", "b", "c", "d", "e", "f",
+                "g");
+        Set<String> s2 = this.createFromArgsTest("a");
+        Set<String> s3 = this.createFromArgsRef("b", "c", "d", "e", "f", "g");
+        s2.add(s3);
+        assertEquals(s1, s2);
+    }
+
+    //--------------- remove tests ---------------
+    /**
+     * test of remove("a") with this = <"a">.
+     */
+    @Test
+    public final void testRemove1() {
+        Set<String> s1 = this.createFromArgsRef();
+        Set<String> s2 = this.createFromArgsTest("a");
+        String str1 = "a";
+        String str2 = s2.remove("a");
+        assertEquals(s1, s2);
+        assertEquals(str1, str2);
+    }
+
+    /**
+     * test of remove("a") with this = <"a", "b", "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testRemove2() {
+        Set<String> s1 = this.createFromArgsRef("b", "c", "d", "e", "f", "g");
+        Set<String> s2 = this.createFromArgsTest("a", "b", "c", "d", "e", "f",
+                "g");
+        String str1 = "a";
+        String str2 = s2.remove("a");
+        assertEquals(s1, s2);
+        assertEquals(str1, str2);
+    }
+
+    /**
+     * test of remove(<"a">) with this = <"a">.
+     */
+    @Test
+    public final void testRemove3() {
+        Set<String> s1 = this.createFromArgsRef();
+        Set<String> s2 = this.createFromArgsTest("a");
+        Set<String> s3 = this.createFromArgsRef("a");
+        Set<String> s4 = s2.remove(s3);
+        assertEquals(s1, s2);
+        assertEquals(s3, s4);
+    }
+
+    /**
+     * test of remove(<"b", "c", "d", "e", "f", "g">) with this = <"a", "b",
+     * "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testRemove4() {
+        Set<String> s1 = this.createFromArgsRef("a");
+        Set<String> s2 = this.createFromArgsTest("a", "b", "c", "d", "e", "f",
+                "g");
+        Set<String> s3 = this.createFromArgsRef("b", "c", "d", "e", "f", "g");
+        Set<String> s4 = s2.remove(s3);
+        assertEquals(s1, s2);
+        assertEquals(s3, s4);
+    }
+
+    //--------------- removeAny tests ---------------
+    /**
+     * test of removeAny() with this = <"a">.
+     */
+    @Test
+    public final void testRemoveAny1() {
+        Set<String> s1 = this.createFromArgsRef("a");
+        Set<String> s2 = this.createFromArgsTest("a");
+        String str2 = s2.removeAny();
+        String str1 = s1.remove(str2);
+        assertEquals(s1, s2);
+        assertEquals(str1, str2);
+    }
+
+    /**
+     * test of removeAny() with this = <"a", "b", "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testRemoveAny2() {
+        Set<String> s1 = this.createFromArgsRef("a", "b", "c", "d", "e", "f",
+                "g");
+        Set<String> s2 = this.createFromArgsTest("a", "b", "c", "d", "e", "f",
+                "g");
+        String str2 = s2.removeAny();
+        String str1 = s1.remove(str2);
+        assertEquals(s1, s2);
+        assertEquals(str1, str2);
+    }
+
+    //--------------- contains tests ---------------
+    /**
+     * test of contains("a") with this = <"a">.
+     */
+    @Test
+    public final void testContains1() {
+        Set<String> s1 = this.createFromArgsRef("a");
+        Set<String> s2 = this.createFromArgsTest("a");
+        boolean b = s2.contains("a");
+        assertEquals(s1, s2);
+        assertTrue(b);
+    }
+
+    /**
+     * test of contains("a") with this = <>.
+     */
+    @Test
+    public final void testContains2() {
+        Set<String> s1 = this.createFromArgsRef();
+        Set<String> s2 = this.createFromArgsTest();
+        boolean b = s2.contains("a");
+        assertEquals(s1, s2);
+        assertTrue(!b);
+    }
+
+    /**
+     * test of contains("a") with this = <"a", "b", "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testContains3() {
+        Set<String> s1 = this.createFromArgsRef("a", "b", "c", "d", "e", "f",
+                "g");
+        Set<String> s2 = this.createFromArgsTest("a", "b", "c", "d", "e", "f",
+                "g");
+        boolean b = s2.contains("a");
+        assertEquals(s1, s2);
+        assertTrue(b);
+    }
+
+    /**
+     * test of contains("a") with this = <"b", "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testContains4() {
+        Set<String> s1 = this.createFromArgsRef("b", "c", "d", "e", "f", "g");
+        Set<String> s2 = this.createFromArgsTest("b", "c", "d", "e", "f", "g");
+        boolean b = s2.contains("a");
+        assertEquals(s1, s2);
+        assertTrue(!b);
+    }
+
+    //--------------- size tests ---------------
+    /**
+     * test of size() with this = <>.
+     */
+    @Test
+    public final void testSize1() {
+        Set<String> s1 = this.createFromArgsRef();
+        Set<String> s2 = this.createFromArgsTest();
+        int size1 = 0;
+        int size2 = s2.size();
+        assertEquals(s1, s2);
+        assertEquals(size1, size2);
+    }
+
+    /**
+     * test of size() with this = <"a">.
+     */
+    @Test
+    public final void testSize2() {
+        Set<String> s1 = this.createFromArgsRef("a");
+        Set<String> s2 = this.createFromArgsTest("a");
+        int size1 = 1;
+        int size2 = s2.size();
+        assertEquals(s1, s2);
+        assertEquals(size1, size2);
+    }
+
+    /**
+     * test of size() with this = <"a", "b", "c", "d", "e", "f", "g">.
+     */
+    @Test
+    public final void testSize3() {
+        Set<String> s1 = this.createFromArgsRef("a", "b", "c", "d", "e", "f",
+                "g");
+        Set<String> s2 = this.createFromArgsTest("a", "b", "c", "d", "e", "f",
+                "g");
+        int size1 = 7;
+        int size2 = s2.size();
+        assertEquals(s1, s2);
+        assertEquals(size1, size2);
     }
 
 }
