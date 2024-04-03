97fb6b282847859cfbdfae7ade1a59d54db376df
AA-maker <57161843+AA-maker@users.noreply.github.com>
2024-04-03 16:23:26 -0400

    Update Program1Parse1.java

--- workspace copy/BLParser/src/Program1Parse1.java
+++ workspace copy/BLParser/src/Program1Parse1.java
@@ -1,3 +1,4 @@
+import components.map.Map;
 import components.program.Program;
 import components.program.Program1;
 import components.queue.Queue;
@@ -6,12 +7,13 @@ import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
 import components.simplewriter.SimpleWriter1L;
 import components.statement.Statement;
+import components.utilities.Reporter;
 import components.utilities.Tokenizer;
 
 /**
  * Layered implementation of secondary method {@code parse} for {@code Program}.
  *
- * @author Put your name here
+ * @author Akshay Anand and Alexander Nistor
  *
  */
 public final class Program1Parse1 extends Program1 {
@@ -56,10 +58,28 @@ public final class Program1Parse1 extends Program1 {
         assert tokens.length() > 0 && tokens.front().equals("INSTRUCTION") : ""
                 + "Violation of: <\"INSTRUCTION\"> is proper prefix of tokens";
 
-        // TODO - fill in body
+        //Checks if identifier and tokens are valid, if not then it sends an error
+        String start = tokens.dequeue();
+        Reporter.assertElseFatalError(Tokenizer.isIdentifier(tokens.dequeue()),
+                "Invalid Identifier");
+        Reporter.assertElseFatalError(tokens.dequeue().equals("IS"),
+                "Invalid Token");
 
-        // This line added just to make the program compilable.
-        return null;
+        //Parse block
+        body.parseBlock(tokens);
+
+        //Checks if last 2 tokens are the correct token and identifier at the
+        //end of the queue
+        Reporter.assertElseFatalError(tokens.dequeue().equals("END"),
+                "Invalid Token");
+        String end = tokens.dequeue();
+        Reporter.assertElseFatalError(Tokenizer.isIdentifier(end),
+                "Invalid Identifier");
+        Reporter.assertElseFatalError(start.equals(end),
+                "More than one identifier used for instruction name");
+
+        //Returns identifier
+        return start;
     }
 
     /*
@@ -91,7 +111,65 @@ public final class Program1Parse1 extends Program1 {
         assert tokens.length() > 0 : ""
                 + "Violation of: Tokenizer.END_OF_INPUT is a suffix of tokens";
 
-        // TODO - fill in body
+        //Checks conditions for first 3 values in tokens queue
+        Reporter.assertElseFatalError(tokens.dequeue().equals("PROGRAM"),
+                "Invalid Token");
+
+        String start = tokens.dequeue();
+        Reporter.assertElseFatalError(Tokenizer.isIdentifier(start),
+                "Invalid Identifier");
+
+        Reporter.assertElseFatalError(tokens.dequeue().equals("IS"),
+                "Invalid Token");
+
+        //Initialize map for contexts for use later
+        Map<String, Statement> context = this.newContext();
+
+        //While-loop parses all instructions
+        while (!tokens.front().equals("BEGIN")) {
+
+            //Initialize temp body variable
+            Statement tempBody = this.newBody();
+
+            //Stores instruction received from outside method
+            String instruction = parseInstruction(tokens, tempBody);
+
+            //Checks identifier required properties
+            Reporter.assertElseFatalError(!Tokenizer.isKeyword(instruction),
+                    "Keyword cannot be an identifier");
+            Reporter.assertElseFatalError(!context.hasKey(instruction),
+                    "Cannot use duplicate identifer as instruction name");
+
+            //Adds instruction name and body to context
+            context.add(instruction, tempBody);
+        }
+
+        //Checks beginning to be valid
+        Reporter.assertElseFatalError(tokens.dequeue().equals("BEGIN"),
+                "Invalid Token");
+
+        //Parse tokens and body
+        Statement blockMain = this.newBody();
+        blockMain.parseBlock(tokens);
+
+        //Checks properties of last 3 values in tokens queue
+        Reporter.assertElseFatalError(tokens.dequeue().equals("END"),
+                "Invalid Token");
+
+        String end1 = tokens.dequeue();
+        Reporter.assertElseFatalError(Tokenizer.isIdentifier(end1),
+                "Invalid Identifier");
+        Reporter.assertElseFatalError(start.equals(end1),
+                "Cannot use multiple " + "identifiers as program name");
+
+        String end2 = tokens.dequeue();
+        Reporter.assertElseFatalError(Tokenizer.END_OF_INPUT.equals(end2),
+                "Program does not end correctly");
+
+        //Puts information into this
+        this.setName(start);
+        this.swapBody(blockMain);
+        this.swapContext(context);
 
     }
 
